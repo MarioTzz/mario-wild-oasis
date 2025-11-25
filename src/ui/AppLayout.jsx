faa792bdbc2d4d037078eea4c2610794fb339a18
Mario <tzz1550455954@gmail.com>
2025-11-25 15:28:56 +0800

    fix a bug about sidebar name

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
-import Sidebar from './SiderBar';
+import Sidebar from './Siderbar';
 import styled from 'styled-components';
 
 const StyledAppLayout = styled.div`
